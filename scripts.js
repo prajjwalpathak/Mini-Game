59864e45ce34a0d7b574b040ef4c6d98870cad67
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-09-19 00:57:43 +0530

    Eat Food

--- scripts.js
+++ scripts.js
@@ -1,30 +1,52 @@
-let positionX = 0;
-let positionY = 0;
+const snake = document.getElementById('snake');
+const food = document.getElementById('food');
+
+let snakePositionX = 0;
+let snakePositionY = 0;
 
 let foodPositionX = Math.floor(Math.random() * 40);
 let foodPositionY = Math.floor(Math.random() * 40);
 
-const snake = document.getElementById('snake');
-const food = document.getElementById('food');
+const foodGeneration = () => {
+	food.style.transform = `translate(${foodPositionX}rem, ${foodPositionY}rem)`;
+};
 
-food.style.transform = `translate(${foodPositionX}rem, ${foodPositionY}rem)`;
+const foodGenerationOnCollision = () => {
+	foodPositionX = Math.floor(Math.random() * 40);
+	foodPositionY = Math.floor(Math.random() * 40);
+	foodGeneration();
+};
+
+const checkCollision = () => {
+	let snakeBCR = snake.getBoundingClientRect();
+	let foodBCR = food.getBoundingClientRect();
+	if (snakeBCR.left === foodBCR.left && snakeBCR.right === foodBCR.right && snakeBCR.top === foodBCR.top && snakeBCR.bottom === foodBCR.bottom) {
+		foodGenerationOnCollision();
+	}
+};
+
+document.addEventListener('load', foodGeneration());
 
 document.addEventListener('keydown', (e) => {
 	if (e.key === 'ArrowRight' || e.key === 'd') {
 		// console.log('Move Right');
-		snake.style.transform = `translate(${positionX === 39 ? 0 : positionX + 1}rem, ${positionY}rem)`;
-		positionX = positionX === 39 ? 0 : positionX + 1;
+		snake.style.transform = `translate(${snakePositionX === 39 ? 0 : snakePositionX + 1}rem, ${snakePositionY}rem)`;
+		snakePositionX = snakePositionX === 39 ? 0 : snakePositionX + 1;
 	} else if (e.key === 'ArrowLeft' || e.key === 'a') {
 		// console.log('Move Left');
-		snake.style.transform = `translate(${positionX === 0 ? 39 : positionX - 1}rem, ${positionY}rem)`;
-		positionX = positionX === 0 ? 39 : positionX - 1;
+		snake.style.transform = `translate(${snakePositionX === 0 ? 39 : snakePositionX - 1}rem, ${snakePositionY}rem)`;
+		snakePositionX = snakePositionX === 0 ? 39 : snakePositionX - 1;
 	} else if (e.key === 'ArrowUp' || e.key === 'w') {
 		// console.log('Move Up');
-		snake.style.transform = `translate(${positionX}rem, ${positionY === 0 ? 39 : positionY - 1}rem)`;
-		positionY = positionY === 0 ? 39 : positionY - 1;
+		snake.style.transform = `translate(${snakePositionX}rem, ${snakePositionY === 0 ? 39 : snakePositionY - 1}rem)`;
+		snakePositionY = snakePositionY === 0 ? 39 : snakePositionY - 1;
 	} else if (e.key === 'ArrowDown' || e.key === 's') {
 		// console.log('Move Down');
-		snake.style.transform = `translate(${positionX}rem, ${positionY === 39 ? 0 : positionY + 1}rem)`;
-		positionY = positionY === 39 ? 0 : positionY + 1;
+		snake.style.transform = `translate(${snakePositionX}rem, ${snakePositionY === 39 ? 0 : snakePositionY + 1}rem)`;
+		snakePositionY = snakePositionY === 39 ? 0 : snakePositionY + 1;
 	}
+});
+
+document.addEventListener('keydown', (e) => {
+	checkCollision();
 });
