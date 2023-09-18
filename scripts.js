02c1f20f240484e385be025cdc228a673fbbc171
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-09-19 01:35:48 +0530

    New Feature

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-const snake = document.getElementById('snake');
+const man = document.getElementById('man');
 const food = document.getElementById('food');
 
-snake.style.transform = `translate(19rem,19rem)`;
-let snakePositionX = 19;
-let snakePositionY = 19;
+man.style.transform = `translate(19rem,19rem)`;
+let manPositionX = 19;
+let manPositionY = 19;
 
 let foodPositionX = Math.floor(Math.random() * 40);
 let foodPositionY = Math.floor(Math.random() * 40);
@@ -19,9 +19,9 @@ const foodGenerationOnCollision = () => {
 };
 
 const checkCollision = () => {
-	let snakeBCR = snake.getBoundingClientRect();
+	let manBCR = man.getBoundingClientRect();
 	let foodBCR = food.getBoundingClientRect();
-	if (snakeBCR.left === foodBCR.left && snakeBCR.right === foodBCR.right && snakeBCR.top === foodBCR.top && snakeBCR.bottom === foodBCR.bottom) {
+	if (manBCR.left === foodBCR.left && manBCR.right === foodBCR.right && manBCR.top === foodBCR.top && manBCR.bottom === foodBCR.bottom) {
 		foodGenerationOnCollision();
 	}
 };
@@ -31,20 +31,20 @@ document.addEventListener('load', foodGeneration());
 document.addEventListener('keydown', (e) => {
 	if (e.key === 'ArrowRight' || e.key === 'd') {
 		// console.log('Move Right');
-		snake.style.transform = `translate(${snakePositionX === 39 ? 0 : snakePositionX + 1}rem, ${snakePositionY}rem)`;
-		snakePositionX = snakePositionX === 39 ? 0 : snakePositionX + 1;
+		man.style.transform = `translate(${manPositionX === 39 ? 0 : manPositionX + 1}rem, ${manPositionY}rem)`;
+		manPositionX = manPositionX === 39 ? 0 : manPositionX + 1;
 	} else if (e.key === 'ArrowLeft' || e.key === 'a') {
 		// console.log('Move Left');
-		snake.style.transform = `translate(${snakePositionX === 0 ? 39 : snakePositionX - 1}rem, ${snakePositionY}rem)`;
-		snakePositionX = snakePositionX === 0 ? 39 : snakePositionX - 1;
+		man.style.transform = `translate(${manPositionX === 0 ? 39 : manPositionX - 1}rem, ${manPositionY}rem)`;
+		manPositionX = manPositionX === 0 ? 39 : manPositionX - 1;
 	} else if (e.key === 'ArrowUp' || e.key === 'w') {
 		// console.log('Move Up');
-		snake.style.transform = `translate(${snakePositionX}rem, ${snakePositionY === 0 ? 39 : snakePositionY - 1}rem)`;
-		snakePositionY = snakePositionY === 0 ? 39 : snakePositionY - 1;
+		man.style.transform = `translate(${manPositionX}rem, ${manPositionY === 0 ? 39 : manPositionY - 1}rem)`;
+		manPositionY = manPositionY === 0 ? 39 : manPositionY - 1;
 	} else if (e.key === 'ArrowDown' || e.key === 's') {
 		// console.log('Move Down');
-		snake.style.transform = `translate(${snakePositionX}rem, ${snakePositionY === 39 ? 0 : snakePositionY + 1}rem)`;
-		snakePositionY = snakePositionY === 39 ? 0 : snakePositionY + 1;
+		man.style.transform = `translate(${manPositionX}rem, ${manPositionY === 39 ? 0 : manPositionY + 1}rem)`;
+		manPositionY = manPositionY === 39 ? 0 : manPositionY + 1;
 	}
 });
 
