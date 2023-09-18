9a77d4fa8c4c085c6700295b53a4b6d8e5dffa01
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-09-18 14:09:06 +0530

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let positionX = 0;
+let positionY = 0;
+const snake = document.getElementById('snake');
+
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'ArrowRight' || e.key === 'd') {
+		console.log('Move Right');
+		snake.style.transform = `translate(${positionX === 39 ? 0 : positionX + 1}rem, ${positionY}rem)`;
+		positionX = positionX === 39 ? 0 : positionX + 1;
+	} else if (e.key === 'ArrowLeft' || e.key === 'a') {
+		console.log('Move Left');
+		snake.style.transform = `translate(${positionX === 0 ? 39 : positionX - 1}rem, ${positionY}rem)`;
+		positionX = positionX === 0 ? 39 : positionX - 1;
+	} else if (e.key === 'ArrowUp' || e.key === 'w') {
+		console.log('Move Up');
+		snake.style.transform = `translate(${positionX}rem, ${positionY === 0 ? 39 : positionY - 1}rem)`;
+		positionY = positionY === 0 ? 39 : positionY - 1;
+	} else if (e.key === 'ArrowDown' || e.key === 's') {
+		console.log('Move Down');
+		snake.style.transform = `translate(${positionX}rem, ${positionY === 39 ? 0 : positionY + 1}rem)`;
+		positionY = positionY === 39 ? 0 : positionY + 1;
+	}
+});
